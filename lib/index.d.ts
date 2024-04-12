f30a9e23fca87d98db724cf5bf4db20a9510e642
Erik Golinelli <erik@codekraft.it>
2024-04-12 20:31:16 +0200

    Updates

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import LazyWrapper from "./components/LazyWrapper";
-import LazyImg from "./components/LazyImg";
-import LazyVideo from "./components/LazyVideo";
-import LazyIframe from "./components/LazyIframe";
-import LazyEl from "./components/LazyEl";
-export { LazyWrapper, LazyImg, LazyVideo, LazyIframe, LazyEl };
-export * from "./types";
--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Mixin the lazy class to the element
- * @param className string the class name to add
- * @return string the class name to add to the element
- */
-export function lazyClassName(className?: string): string {
-	return className ? className + " lazy" : "lazy";
-}
