c29529b7c1cec4a8802718c7305f0620082eb527
Erik Golinelli <erik@codekraft.it>
2024-04-13 12:24:22 +0200

    initLazyLoad initializer function that simplifies the component's code + exports updated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import VanillaLazyLoad from "./components/VanillaLazyLoad";
 import LazyWrapper from "./components/LazyWrapper";
 import LazyImg from "./components/LazyImg";
 import LazyVideo from "./components/LazyVideo";
 import LazyIframe from "./components/LazyIframe";
 import LazyEl from "./components/LazyEl";
+import { initLazyLoad } from "./utils";
 
-export { LazyWrapper, LazyImg, LazyVideo, LazyIframe, LazyEl };
+export {
+	initLazyLoad,
+	VanillaLazyLoad,
+	LazyWrapper,
+	LazyImg,
+	LazyVideo,
+	LazyIframe,
+	LazyEl,
+};
 
 export * from "./types";
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,38 @@
+import LazyLoad, { ILazyLoadOptions } from "vanilla-lazyload";
+
+declare global {
+	export interface Window {
+		lazyLoadOptions: ILazyLoadOptions;
+	}
+}
+
+/**
+ * The function to initialize vanilla-lazyload
+ *
+ * @param container - The container element
+ * @param options - The options for vanilla-lazyload
+ */
+export function initLazyLoad(
+	container = document.body,
+	options: ILazyLoadOptions = {},
+) {
+	container.classList?.add("vanilla-lazyload-wrapper");
+
+	// Set options for vanilla-lazyload async mode
+	window.lazyLoadOptions = {
+		...options,
+		container,
+	};
+
+	// Initializing vanilla-lazyload
+	if (typeof window !== "undefined" && "IntersectionObserver" in window) {
+		const vll = new LazyLoad();
+
+		// Destroying vanilla-lazyload when component unmounts
+		return () => vll.destroy();
+	}
+}
+
 /**
  * Mixin the lazy class to the element
  * @param className string the class name to add
