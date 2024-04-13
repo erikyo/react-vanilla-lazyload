9e6550e5a310e4f6646b86c4dbcf1cc3718c82ec
Erik Golinelli <erik@codekraft.it>
2024-04-13 18:03:10 +0200

    Updates

---
+++ assets/LazyImg-BCKEZley.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CrjzJB1l.js";import{l as c}from"./WallPaper-BPyy02D0.js";const i=({src:a,srcSet:s,dataSizes:t,alt:m,className:r,...o})=>e.jsx("img",{"data-src":a,"data-srcset":s,"data-sizes":t,alt:m,className:c(r),...o}),p=i;export{p as L};
---
+++ assets/VanillaLazyLoad-DQmuKvE7.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-CrjzJB1l.js";import{i as r}from"./WallPaper-BPyy02D0.js";const t=({options:a})=>(o.useEffect(()=>{r()},[a]),null),s=t;export{s as V};
