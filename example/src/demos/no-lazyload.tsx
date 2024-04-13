802ab11cc2c03903559f70d3160e342ddfe37eef
Erik Golinelli <erik@codekraft.it>
2024-04-13 15:55:06 +0200

    no lazyload demo pages for comparison

---
+++ example/src/demos/no-lazyload.tsx
@@ -0,0 +1,20 @@
+import { VanillaLazyLoad } from "react-vanilla-lazyload";
+import "animate.css/animate.min.css";
+import WallPaper from "../components/WallPaper.tsx";
+
+export default function ImagesLL() {
+	return (
+		<>
+			<WallPaper />
+			{[...Array(50).keys()].map((index) => (
+				<img
+					className={"demo-item"}
+					key={index}
+					loading={"lazy"}
+					src={`https://verlok.github.io/vanilla-lazyload/demos/images/440x560-${String(index + 1).padStart(2, "0")}.webp`}
+				/>
+			))}
+			<VanillaLazyLoad />
+		</>
+	);
+}
