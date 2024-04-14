1bd6bbef0f754507a7f206f68e54ec09e95e7a57
Erik Golinelli <erik@codekraft.it>
2024-04-14 18:11:11 +0200

    tests env

--- vitest.ts
+++ vitest.ts
@@ -1,9 +1,14 @@
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+	plugins: [react()],
 	test: {
 		environment: "jsdom",
+		root: "src",
+		clearMocks: true,
 		globals: true,
-		setupFiles: "tests/setUpTests.tsx",
+		include: ["tests/*.test.{tsx,ts}"],
+		setupFiles: "tests/setup.ts",
 	},
 });
