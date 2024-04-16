1f871f655c5bf3d6c17f5a7f3474c914ff2725d4
Erik Golinelli <erik@codekraft.it>
2024-04-16 12:52:51 +0200

    Lazymodule

--- src/components/LazyModule.tsx
+++ src/components/LazyModule.tsx
@@ -1,17 +1,22 @@
-import {
-	type ComponentType,
-	type Dispatch,
-	type LazyExoticComponent,
-	type SetStateAction,
-	Suspense,
-	lazy,
-	useEffect,
-	useRef,
-	useState,
-} from "react";
+import { Suspense, lazy, useEffect, useRef, useState } from "react";
 import type { LazyModuleProps } from "../types";
 import { lazyClassName } from "../utils";
 
+/**
+ * The enum for the visibility state of an element
+ */
+enum Visibility {
+	hidden = "hidden",
+	visible = "visible",
+}
+
+/**
+ * The function to observe changes in the class attribute of an element and trigger a callback
+ * In the callback we can replace the element with the lazy-loaded component
+ *
+ * @param targetNode - The element to observe
+ * @param handler - The callback to trigger
+ */
 function mutationObserver(targetNode: Node, handler: () => void) {
 	const config = { attributeFilter: ["class"] };
 
@@ -26,17 +31,31 @@ function mutationObserver(targetNode: Node, handler: () => void) {
 	return () => observer.disconnect();
 }
 
+/**
+ * The simple Loader component
+ * @constructor
+ */
+function Loader(): JSX.Element {
+	return <p>{"loading"}</p>;
+}
+
+/**
+ * The LazyModule component is used to lazy load modules and components
+ * @constructor
+ *
+ * @param component - The component to lazy load
+ * @param className - The class name of the object
+ * @param loader - The loader component
+ * @param props - The props for the component
+ */
 const LazyModule = ({
 	component,
 	className,
 	loader,
 	...props
 }: LazyModuleProps): JSX.Element => {
-	const [LazyComponent, setLazyComponent] = useState(null) as [
-		LazyExoticComponent<ComponentType<unknown>> | null,
-		Dispatch<SetStateAction<LazyExoticComponent<ComponentType<unknown>>>>,
-	];
-
+	const [visible, setVisible] = useState(Visibility.hidden);
+	/** @var myRef - The reference to the object */
 	const myRef = useRef<HTMLObjectElement>(null);
 
 	useEffect(() => {
@@ -45,21 +64,31 @@ const LazyModule = ({
 		}
 	}, []);
 
+	/**
+	 * handleClassChange is a callback that is triggered when the class attribute of the object changes to active
+	 */
 	const handleClassChange = () => {
-		const lazyCb = lazy(component);
-		setLazyComponent(lazyCb);
+		if (myRef.current) setVisible(Visibility.visible);
 	};
 
-	if (LazyComponent) {
+	/**
+	 * Whenever the element is "visible" we can lazily load the component
+	 */
+	if (visible === Visibility.visible) {
+		const LazyComponent = lazy(component);
 		return (
-			<Suspense fallback={loader ?? <p>{"loading"}</p>}>
+			<Suspense fallback={loader ?? <Loader />}>
 				<LazyComponent {...props} />
 			</Suspense>
 		);
 	}
+
+	/**
+	 * Otherwise, we render the loader component
+	 */
 	return (
 		<object type="module" ref={myRef} className={lazyClassName(className)}>
-			proxy
+			{loader ?? <Loader />}
 		</object>
 	);
 };
