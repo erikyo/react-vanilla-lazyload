ce0a6298bef58a9ef1c34d01d1bb92b4ba60ff4c
Erik Golinelli <erik@codekraft.it>
2024-04-17 13:31:42 +0200

    optionalDependencies (for testing)

--- tests/wrapper.test.tsx
+++ tests/wrapper.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, it, vitest } from "vitest";
+import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
 import { LazyImg, LazyWrapper } from "../src";
 
 describe("LazyWrapper component", () => {
